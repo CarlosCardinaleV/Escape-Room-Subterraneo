a6687bd40933474e4b0eddf0c7431d86021687ce
Carlos Cardinale <carlos.cardinale@ucr.ac.cr>
2023-10-23 22:26:43 -0600

    adding documentation to the class Hechizo

--- src/Hechizo.py
+++ src/Hechizo.py
@@ -3,25 +3,80 @@ import Cerrojo
 
 class Hechizo:
     def __init__(self, segundos=1.0, contador=0):
+        """
+        Inicializa una instancia de Hechizo.
+
+        Args:
+            segundos (float): La duración en segundos del hechizo
+            (por defecto, 1.0).
+            contador (int): El contador del hechizo (por defecto, 0).
+        """
         self.segundosHechizo = segundos
         self.contadorHechizo = contador
 
     def setContadorHechizo(self, contador):
+        """
+        Establece el contador del hechizo.
+
+        Args:
+            contador (int): El valor del contador a establecer.
+
+        Returns:
+            None
+        """
         self.contadorHechizo += contador
 
     def getContadorHechizo(self):
+        """
+        Obtiene el valor del contador del hechizo.
+
+        Returns:
+            int: El valor del contador del hechizo.
+        """
         return self.contadorHechizo
 
     def setSegundosHechizo(self, segundos):
+        """
+        Establece la duración del hechizo en segundos.
+
+        Args:
+            segundos (float): La duración del hechizo en segundos
+            (debe ser igual o mayor a 1.0).
+
+        Returns:
+            None
+        """
         if segundos >= 1.0:
             self.segundosHechizo = segundos
         else:
             self.segundosHechizo = 1.0
 
     def getSegundosHechizo(self):
+        """
+        Obtiene la duración en segundos del hechizo.
+
+        Returns:
+            float: La duración en segundos del hechizo.
+        """
         return self.segundosHechizo
 
     def vidaEnfrentamientoJugadorCerrojo(self, jugador, cerrojo, cualGemaJugador, cualGemaCerrojo):
+        """
+        Realiza un enfrentamiento entre un jugador y un cerrojo usando un hechizo. Calcula los
+        cambios en la vida de las gemas
+        involucradas en el enfrentamiento.
+
+        Args:
+            jugador (Jugador): El jugador que realiza el hechizo.
+            cerrojo (Cerrojo): El cerrojo objetivo del hechizo.
+            cualGemaJugador (int): El número de la gema del jugador involucrada en el
+            enfrentamiento (1, 2 o 3).
+            cualGemaCerrojo (int): El número de la gema del cerrojo involucrada en el
+            enfrentamiento (1, 2 o 3).
+
+        Returns:
+            None
+        """
         vidaGemaJugador = jugador.getGemaJugador(cualGemaJugador).getVidaGema()
         vidaGemaCerrojo = cerrojo.getGemaCerrojo(cualGemaCerrojo).getVidaGema()
         ataqueGemaJugador = jugador.getGemaJugador(cualGemaJugador).getAtaqueGema()
